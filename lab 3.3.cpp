ebd634695eee4339f7c46b3765de2ee701373ee1
Vikysja <91226103+Vikysja@users.noreply.github.com>
2021-10-05 23:48:16 +0300

    Add files via upload

---
+++ lab 3.3.cpp
@@ -0,0 +1,39 @@
+﻿// lab_03_3.cpp
+// Квецко Вікторії 
+// Лабораторна робота № 3.3
+// Розгалуження, задане графіком функції.
+// Варіант 11
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double x; // вхідний аргумент
+	double R; // вхідний параметр
+	double y; // результат обчислення виразу
+
+	cout << "R = "; cin >> R;
+	cout << "x = "; cin >> x;
+
+	// розгалуження в повній формі
+	if (x<= - 1 - R)
+		y = -x - 1 - R ;
+	else
+		if (-1-R < x && x <= -1)
+			y = pow(((R + x + 1) * (R - x - 1)), 0.5);
+		else
+			if (-1  < x && x <= 1)
+				y = R;
+			else
+				if (1 < x && x <= 2)
+					y = 2*R-x+1-R*x;
+				else
+					y = -1;
+
+	cout << endl;
+	cout << "y = " << y << endl;
+	cin.get();
+	return 0;
+}
